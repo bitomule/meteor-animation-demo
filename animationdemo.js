fc90fa46c310ef58ba7658ad6bfcc5002af3da1b
David Collado <bitomule@gmail.com>
2014-08-01 10:59:26 +0000

    Animacion funcionando

--- animationdemo.js
+++ animationdemo.js
@@ -1,23 +1,6 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to animationdemo.";
-  };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-	
+	Session.setDefault('exampleArray',[]);
 	Template.hello.exampleArray = function(){
-		return ['',''];
+		return Session.get('exampleArray');
 	};
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
